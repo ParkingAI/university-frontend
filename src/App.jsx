987a88c60fa7ce2577ca02f25aa9d073573aa2c3
Ronan <ronan.krebel@gmail.com>
2026-01-20 20:24:16 +0100

    Frontend (heroui) - init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@heroui/react'
+function App() {
+
+  return (
+    
+    <div className='text-center'>
+      <h1 className='text-3xl mb-4'>Parkađoko</h1>
+      <Button color='primary'>test</Button>
+
+    </div>
+  )
+}
+
+export default App
